5f757f3475a72382f5739a1627ca49f4f97917a6
Emil Pana <emil.pana@softvision.com>
2021-01-15 17:20:55 +0200

    Added bottom nav bar. Changed the theme style. Added blank fragments and layouts. Added nav graph.

---
+++ app/src/main/java/com/emilpana/directoryapp/ui/fragment/PeopleFragment.kt
@@ -0,0 +1,36 @@
+package com.emilpana.directoryapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.emilpana.directoryapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PeopleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PeopleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_people, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        val navController = findNavController()
+        toolbar.setupWithNavController(navController)
+    }
+}

afb7831eaf787bb6e80c6dc84f5b79de0aaf4e9f
Emil Pana <emil.pana@softvision.com>
2021-01-28 13:26:46 +0200

    Fixed UTs.

--- app/src/test/java/com/emilpana/directoryapp/mock/MockDatabase.kt
+++ app/src/test/java/com/emilpana/directoryapp/mock/MockDatabase.kt
@@ -5,62 +5,64 @@ import com.emilpana.directoryapp.data.local.database.PersonDao
 import com.emilpana.directoryapp.data.local.database.RoomDao
 import com.emilpana.directoryapp.data.local.entity.LocalPerson
 import com.emilpana.directoryapp.data.local.entity.LocalRoom
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
 
 object MockDatabase : DaoProvider {
     override fun personDao(): PersonDao {
         return object : PersonDao {
-            override fun getAllPersons(): List<LocalPerson> {
-                return listOf(
+            override fun getAllPersons(): Single<List<LocalPerson>> {
+                return Single.just(
+                    listOf(
+                        LocalPerson(
+                            avatar = "avatar1",
+                            createdAt = "createdAt1",
+                            email = "email1",
+                            favouriteColor = "favouriteColor1",
+                            firstName = "firstName1",
+                            id = "id1",
+                            jobTitle = "jobTitle1",
+                            lastName = "lastName1",
+                            latitude = 1.0,
+                            longitude = 2.0,
+                            phone = "phone1"
+                        ),
+                        LocalPerson(
+                            avatar = "avatar2",
+                            createdAt = "createdAt2",
+                            email = "email2",
+                            favouriteColor = "favouriteColor2",
+                            firstName = "firstName2",
+                            id = "id2",
+                            jobTitle = "jobTitle2",
+                            lastName = "lastName2",
+                            latitude = 2.0,
+                            longitude = 3.0,
+                            phone = "phone2"
+                        )
+                    )
+                )
+            }
+
+            override fun insertAll(person: List<LocalPerson>) = Completable.complete()
+
+            override fun deleteAllPersons() = Completable.complete()
+
+            override fun getPersonById(personId: String): Single<LocalPerson> {
+                return Single.just(
                     LocalPerson(
                         avatar = "avatar1",
                         createdAt = "createdAt1",
                         email = "email1",
                         favouriteColor = "favouriteColor1",
                         firstName = "firstName1",
-                        id = "id1",
+                        id = "id$personId",
                         jobTitle = "jobTitle1",
                         lastName = "lastName1",
                         latitude = 1.0,
                         longitude = 2.0,
                         phone = "phone1"
-                    ),
-                    LocalPerson(
-                        avatar = "avatar2",
-                        createdAt = "createdAt2",
-                        email = "email2",
-                        favouriteColor = "favouriteColor2",
-                        firstName = "firstName2",
-                        id = "id2",
-                        jobTitle = "jobTitle2",
-                        lastName = "lastName2",
-                        latitude = 2.0,
-                        longitude = 3.0,
-                        phone = "phone2"
                     )
-                )
-            }
-
-            override fun insertAll(person: List<LocalPerson>) {
-
-            }
-
-            override fun deleteAllPersons() {
-
-            }
-
-            override fun getPersonById(personId: String): LocalPerson {
-                return LocalPerson(
-                    avatar = "avatar1",
-                    createdAt = "createdAt1",
-                    email = "email1",
-                    favouriteColor = "favouriteColor1",
-                    firstName = "firstName1",
-                    id = "id$personId",
-                    jobTitle = "jobTitle1",
-                    lastName = "lastName1",
-                    latitude = 1.0,
-                    longitude = 2.0,
-                    phone = "phone1"
                 )
             }
         }
@@ -68,32 +70,30 @@ object MockDatabase : DaoProvider {
 
     override fun roomDao(): RoomDao {
         return object : RoomDao {
-            override fun getAllRooms(): List<LocalRoom> {
-                return listOf(
-                    LocalRoom(
-                        createdAt = "createdAt1",
-                        id = "id2",
-                        isOccupied = false,
-                        maxOccupancy = 1,
-                        name = "nam1"
-                    ),
-                    LocalRoom(
-                        createdAt = "createdAt2",
-                        id = "id2",
-                        isOccupied = true,
-                        maxOccupancy = 2,
-                        name = "name2"
+            override fun getAllRooms(): Single<List<LocalRoom>> {
+                return Single.just(
+                    listOf(
+                        LocalRoom(
+                            createdAt = "createdAt1",
+                            id = "id2",
+                            isOccupied = false,
+                            maxOccupancy = 1,
+                            name = "nam1"
+                        ),
+                        LocalRoom(
+                            createdAt = "createdAt2",
+                            id = "id2",
+                            isOccupied = true,
+                            maxOccupancy = 2,
+                            name = "name2"
+                        )
                     )
                 )
             }
 
-            override fun insertAll(person: List<LocalRoom>) {
+            override fun insertAll(person: List<LocalRoom>) = Completable.complete()
 
-            }
-
-            override fun deleteAllRooms() {
-
-            }
+            override fun deleteAllRooms() = Completable.complete()
         }
     }
 }
